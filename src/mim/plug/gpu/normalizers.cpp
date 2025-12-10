c9f66c1791a89cf64ea9c6c4195ff640fc90ff21
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-12-10 02:12:23 +0100

    chore: remove copied and potentially unnecessary normalizer

--- src/mim/plug/gpu/normalizers.cpp
+++ src/mim/plug/gpu/normalizers.cpp
@@ -4,18 +4,6 @@
 
 namespace mim::plug::gpu {
 
-const Def* normalize_store(const Def*, const Def*, const Def* arg) {
-    auto [mem, ptr, val] = arg->projs<3>();
-
-    if (ptr->isa<Bot>() || val->isa<Bot>()) return mem;
-    if (auto pack = val->isa<Pack>(); pack && pack->body()->isa<Bot>()) return mem;
-    if (auto tuple = val->isa<Tuple>()) {
-        if (std::ranges::all_of(tuple->ops(), [](const Def* op) { return op->isa<Bot>(); })) return mem;
-    }
-
-    return {};
-}
-
-MIM_gpu_NORMALIZER_IMPL
+// MIM_gpu_NORMALIZER_IMPL
 
 } // namespace mim::plug::gpu
