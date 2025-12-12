bac90e849b1b72c574a0ba5a357f52028a0c8e8f
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-12-12 15:57:34 +0100

    tests: cli and simple lit tests for gpu plugin

---
+++ test.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+mim=/home/friedrich/studium/master/mimir/build/bin/mim
+
+name=kernel_detection
+
+if [[ $# -gt 1 ]]; then
+    name=$1
+fi
+
+error () {
+    echo "$1"
+    exit 1
+}
+
+test_path=/home/friedrich/studium/master/mimir/lit/gpu/$name.mim
+host_ll_path=/home/friedrich/studium/master/mimir/build/test/gpu/Output/$name.mim.tmp.ll 
+dev_ll_path=/home/friedrich/studium/master/mimir/build/test/gpu/Output/$name.mim.tmp.dev.ll 
+hostobj_path=host.obj
+hostbin_path=host
+ptx_path=mimir.ptx
+cubin_path=mimir.cubin
+fatbin=mimir.fatbin
+
+gpu_arch=sm_89
+
+echo "test_path: $test_path"
+echo "host_ll_path: $host_ll_path"
+echo "dev_ll_path: $dev_ll_path"
+
+rm $host_ll_path $dev_ll_path $hostobj_path $hostbin_path $ptx_path $cubin_path $fatbin
+
+$mim $test_path \
+    --output-ll $host_ll_path \
+    --output-device-ll $dev_ll_path \
+    --device-target nvptx \
+    -o - \
+    ${@:2} \
+    || error "mim step"
+
+echo
+echo "-----  HOST LLVM  -----"
+[[ -f $host_ll_path ]] && cat $host_ll_path
+echo "----- DEVICE LLVM -----"
+[[ -f $dev_ll_path ]] && cat $dev_ll_path
+
+echo "----- COMPILATION -----"
+llc -filetype=obj -relocation-model=pic $host_ll_path -o $hostobj_path || error "host object"
+
+clang $hostobj_path -o $hostbin_path -lcuda || error "host binary"
+
+llc -march=nvptx64 -mcpu=$gpu_arch $dev_ll_path -o $ptx_path || error "llc step"
+
+ptxas -arch=$gpu_arch $ptx_path -o $cubin_path || error "ptxas step"
+
+nvcc -fatbin $cubin_path -o $fatbin -arch=$gpu_arch || error "fatbin step"
+
+echo "----- EXECUTE BIN -----"
+args="2 3 4 5 6"
+echo "executing './$hostbin_path $args'"
+echo ">>>"
+./$hostbin_path $args
+echo "<<< rv = $(echo $?)"
+echo "-----------------------"
