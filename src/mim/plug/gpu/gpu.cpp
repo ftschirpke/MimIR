b8b4083b55dbbc4ee88383b002fb7bd5525ea066
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-12-12 15:49:55 +0100

    feat: minimal gpu plugin

---
+++ src/mim/plug/gpu/gpu.cpp
@@ -0,0 +1,10 @@
+#include "mim/plug/gpu/gpu.h"
+
+#include <mim/pass.h>
+#include <mim/plugin.h>
+
+using namespace mim;
+using namespace mim::plug;
+
+/// Registers normalizers as well as Phase%s and Pass%es for the Axm%s of this Plugin.
+extern "C" MIM_EXPORT Plugin mim_get_plugin() { return {"gpu", nullptr, nullptr, nullptr}; }
