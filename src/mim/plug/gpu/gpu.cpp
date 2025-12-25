529cb15accf65ecb48498acd4a88e61df1d17005
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-12-25 20:09:06 +0100

    feat: minimal gpu plugin

---
+++ src/mim/plug/gpu/gpu.cpp
@@ -0,0 +1,8 @@
+#include "mim/plug/gpu/gpu.h"
+
+#include <mim/pass.h>
+#include <mim/plugin.h>
+
+using namespace mim;
+
+extern "C" MIM_EXPORT Plugin mim_get_plugin() { return {"gpu", nullptr, nullptr, nullptr}; }
