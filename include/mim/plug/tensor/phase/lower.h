cb6a5cc74f1ddefe05bef6d5eb0fbeb41dda65d6
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2026-04-27 14:38:38 +0200

    Also lower tensor.map_reduce in phase.
    Also fix issues where the old world was still used to create new defs inside the lowering phase.

--- include/mim/plug/tensor/phase/lower.h
+++ include/mim/plug/tensor/phase/lower.h
@@ -15,6 +15,9 @@ private:
     const Def* lower_get(const App*);
     const Def* lower_set(const App*);
     const Def* lower_broadcast(const App*);
+    const Def* lower_map_reduce(const App*);
+
+    const Def* rec_broadcast(const Def* s_in, const Def* s_out, const Def* input, u64 r, u64 i);
 };
 
 } // namespace mim::plug::tensor
