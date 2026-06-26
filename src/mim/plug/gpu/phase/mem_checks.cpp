c70c0d05961271e54bc5706884cbe29f70076937
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2026-06-26 13:56:58 +0200

    feat: phases check correct usage of machine states and syncs

---
+++ src/mim/plug/gpu/phase/mem_checks.cpp
@@ -0,0 +1,85 @@
+#include "mim/plug/gpu/phase/mem_checks.h"
+
+#include <mim/axm.h>
+
+#include <mim/plug/mem/mem.h>
+
+namespace mim::plug::gpu::phase {
+
+class MemFinder {
+public:
+    MemFinder(const Def* to_search)
+        : to_search({to_search}) {}
+    MemFinder(DefVec&& to_search)
+        : to_search(std::move(to_search)) {}
+
+    using IsaMem = Axm::IsA<mem::M, mim::App>;
+
+    IsaMem next_mem();
+
+private:
+    DefVec to_search;
+};
+
+MemFinder::IsaMem MemFinder::next_mem() {
+    while (!to_search.empty()) {
+        auto cur = to_search.back();
+        to_search.pop_back();
+        if (cur->isa<Sigma>())
+            for (auto op : cur->ops())
+                to_search.push_back(op);
+        else if (auto mem = Axm::isa<mem::M>(cur))
+            return mem;
+    }
+    return IsaMem();
+}
+
+const Def* MemChecks::rewrite_imm_App(const App* app) {
+    if (auto launch = Axm::isa<gpu::launch>(app)) {
+        auto kernel        = launch->decurry()->decurry()->arg();
+        auto kernel_args   = launch->decurry()->arg();
+        auto kernel_args_t = kernel_args->type();
+
+        MemFinder mem_finder(kernel_args_t);
+        if (mem_finder.next_mem()) {
+            error("You may not pass any %mem.M across device boundaries: passing {} : {} from host to kernel '{}'",
+                  kernel_args, kernel_args_t, kernel);
+        }
+    } else if (auto init = Axm::isa<gpu::init>(app)) {
+        auto [_, global_syms, const_syms] = init->args<3>();
+
+        MemFinder global_mem_finder(global_syms);
+        if (global_mem_finder.next_mem()) {
+            error("You may not pass any %mem.M across device boundaries: creating symbol(s) {} in global address space "
+                  "with {}",
+                  global_syms, app);
+        }
+        MemFinder const_mem_finder(const_syms);
+        if (const_mem_finder.next_mem()) {
+            error("You may not pass any %mem.M across device boundaries: creating symbol(s) {} in constant address "
+                  "space with {}",
+                  const_syms, app);
+        }
+    }
+    return Super::rewrite_imm_App(app);
+}
+
+void MemChecks::rewrite_external(Def* def) {
+    auto lam = def->isa<Lam>();
+    if (lam && lam->sym().str() == "main") {
+        MemFinder intype_mem_finder(lam->type()->dom());
+        while (auto mem = intype_mem_finder.next_mem()) {
+            auto addr_space = mem->arg();
+            if (Lit::as(addr_space) != 0) error("The main function may not take %mem.M n with a non-zero n as an argument");
+        }
+
+        MemFinder outtype_mem_finder(lam->type()->ret_dom());
+        while (auto mem = outtype_mem_finder.next_mem()) {
+            auto addr_space = mem->arg();
+            if (Lit::as(addr_space) != 0) error("The main function may not return any %mem.M n with a non-zero n");
+        }
+    }
+    return Super::rewrite_external(def);
+}
+
+} // namespace mim::plug::gpu::phase
